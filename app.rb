ae54efeb223d356e75ca9277d673c9479a7f20a4
Titus Fortner <titusfortner@gmail.com>
2017-11-23 13:57:32 -0600

    implement single attribute

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require_relative 'app/helpers/builder'
+
+include XPathify
 
 get '/' do
   erb :index
@@ -8,5 +11,6 @@ post '/xpath' do
   content_type :json
   data = JSON.parse(request.body.read)
   selectors = data.inject({}) { |h, v| h[v['name']] = v['value']; h}
-  selectors.to_json
+  kv = {selectors['attrname1'] => selectors['attrvalue1']}
+  Builder.build(kv).to_json
 end
