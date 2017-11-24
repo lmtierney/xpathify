67366faf8b11c7ef3ccab7ba8a6c4f8b10c7520c
Titus Fortner <titusfortner@gmail.com>
2017-11-24 15:01:21 -0600

    dynamically create more fields

--- spec/helpers/builder_spec.rb
+++ spec/helpers/builder_spec.rb
@@ -5,15 +5,17 @@ require 'rspec'
 
 module XPathify
   describe Parser do
-    it "builds xpath for multiple attributes" do
-      array = [{"name" => "attrname1", "value" => "foo"},
-              {"name" => "attrvalue1", "value" => "bar"},
-              {"name" => "attrname2", "value" => "foo2"},
-              {"name" => "attrvalue2", "value" => "bar2"},
-              {"name" => "attrname3", "value" => "foobar"},
-              {"name" => "attrvalue3", "value" => "barfoo"}]
+    it "parses data for multiple attributes" do
+      array = [{"name" => "attrname", "value" => ""},
+               {"name" => "attrvalue", "value" => ""},
+               {"name" => "attrname1", "value" => "foo"},
+               {"name" => "attrvalue1", "value" => "bar"},
+               {"name" => "attrname2", "value" => "foo2"},
+               {"name" => "attrvalue2", "value" => "bar2"},
+               {"name" => "attrname3", "value" => "foobar"},
+               {"name" => "attrvalue3", "value" => "barfoo"}]
 
-      expect(Parser.parse(array)).to eq("foo"=>"bar", "foo2"=>"bar2", "foobar"=>"barfoo")
+      expect(Parser.parse(array)).to eq("foo" => "bar", "foo2" => "bar2", "foobar" => "barfoo")
     end
   end
 
@@ -25,7 +27,7 @@ module XPathify
     end
 
     it 'builds xpath for multiple attributes' do
-      selector = {"foo"=>"bar", "foo2"=>"bar2", "foobar"=>"barfoo"}
+      selector = {"foo" => "bar", "foo2" => "bar2", "foobar" => "barfoo"}
       expect(Builder.build(selector)).to eq ".//*[@foo='bar' and @foo2='bar2' and @foobar='barfoo']"
     end
   end
